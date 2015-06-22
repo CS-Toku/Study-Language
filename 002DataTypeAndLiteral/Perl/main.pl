8af2604c11828ccfc1f33e4332b913a37baeae56
CS_Toku <cs_toku@sce-toku.jp>
2015-06-22 16:59:12 +0900

    add subject 002 Perl

---
+++ 002DataTypeAndLiteral/Perl/main.pl
@@ -0,0 +1,16 @@
+
+use strict;
+
+my $i = 1;
+my $f = 3.21;
+my $e = 3e5;
+my $h = 0x123;
+my $o = 0123;
+my $s = "string";
+
+my @array = (1,2,3,4);
+# %hash{'Hey'} = 'Hoo!!';
+my %hash = (
+    "Hey" => 'Hoo!!',
+);
+
