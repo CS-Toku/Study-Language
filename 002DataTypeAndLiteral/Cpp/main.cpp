6b5a8386ad50ebfa3086ad2b6106ad8febacf423
CS_Toku <cs_toku@sce-toku.jp>
2015-06-22 14:36:37 +0900

    add subject 002

---
+++ 002DataTypeAndLiteral/Cpp/main.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+
+int main(void){
+
+    int i = 1;
+    long l = 2L;
+    char c = 'a';
+    unsigned int ui = 1U;
+    unsigned long ul = 2UL;
+    //shortとかは略
+    
+    float f = 0.1;
+    double d = 0.2;
+
+    bool b = true||false;
+    
+}
