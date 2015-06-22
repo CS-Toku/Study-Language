2c492d7079986c142831130285bf969e6d6579a0
CS_Toku <cs_toku@sce-toku.jp>
2015-06-22 13:01:41 +0900

    add subject 002

---
+++ 002DataTypeAndLiteral/C/main.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+
+int main(void){
+    char ch = 'a';
+    int i = 1;
+    short s = 2;
+    long l = 3L;
+    unsigned long ul = 3UL;
+    float f = 0.1F;
+    double d = 0.2;
+
+    char *str = "string";
+
+    return 0;
+}
