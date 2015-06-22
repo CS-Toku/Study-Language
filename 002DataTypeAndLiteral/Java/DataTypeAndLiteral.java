778f9f643a0acc01f8fd0849bd59febcd44f531b
CS_Toku <cs_toku@sce-toku.jp>
2015-06-22 16:29:03 +0900

    add subject 002 Java

---
+++ 002DataTypeAndLiteral/Java/DataTypeAndLiteral.java
@@ -0,0 +1,18 @@
+
+public class DataTypeAndLiteral {
+
+    public static void main(String[] args){
+        boolean b = true;
+        int i = 0;
+        long l = 1231L;
+        // byte, short 省略
+        char c = 'c';
+        float f = 1.40239846F;
+        double d = 239048577e10D;
+
+        //ついで
+        char[] str = "string".toCharArray();
+    
+    }
+
+}
