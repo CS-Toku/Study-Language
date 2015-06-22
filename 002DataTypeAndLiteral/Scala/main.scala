89296ae7e8f98f3cbf744cf79bef5ffe98e8744c
CS_Toku <cs_toku@sce-toku.jp>
2015-06-22 17:32:31 +0900

    add subject 002 Scala

---
+++ 002DataTypeAndLiteral/Scala/main.scala
@@ -0,0 +1,14 @@
+
+object Main extends App {
+    
+    val dec = 1
+    val hex = 0x2
+    val l = 2L
+    val f = 1.2
+    val e = 2e2
+    val c = 'a'
+    val s = "test"
+    val bool = true
+    val symbol = 'Hey
+    
+}
