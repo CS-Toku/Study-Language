f610ddcd2a2183353d50bc9e277d5a8e6f66f0d5
CS_Toku <cs_toku@sce-toku.jp>
2015-06-22 14:28:49 +0900

    add subject 002

---
+++ 002DataTypeAndLiteral/CSharp/DataTypeAndLiteral.cs
@@ -0,0 +1,34 @@
+
+class DataTypeAndliteral
+{
+
+
+    static void Main(string[] args)
+    {
+        int i = 1;
+        uint ui = 1U;
+        long l = 2L;
+        ulong ul = 2UL;
+        //byte, sbyte short, ushort 面倒なんで略
+
+        char ch = 'a';
+        float f = 0.1f;
+        double d = 0.2;
+        double e = 6.1e2;
+        decimal m = 3m;
+        bool b = true;
+
+        string str = "string";
+        string mstr = 
+            @"yahooo
+            ""as;dlfkjsdf""
+            a;sldk
+            ";
+        object o = null;
+        var v = 1;
+
+        
+    
+    }
+
+}
