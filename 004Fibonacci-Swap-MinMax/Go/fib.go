89d0e03f00359328381eec4b0158c3017bc1f800
CS_Toku <cs_toku@sce-toku.jp>
2015-06-30 16:35:58 +0900

    subject 004 Go complete

--- 004Fibonacci-Swap-MinMax/Go/fib.go
+++ 004Fibonacci-Swap-MinMax/Go/fib.go
@@ -8,7 +8,19 @@ func main(){
 }
 
 func fib(last_value int){
-    
+    num1 := 1
+    num2 := 0
+
+    if num2 > last_value{
+        return
+    }
+    fmt.Println(num2)
+    for ;num1 < last_value; {
+        fmt.Println(num1)
+        tmp := num1 + num2
+        num2 = num1
+        num1 = tmp
+    }
 }
 
 
